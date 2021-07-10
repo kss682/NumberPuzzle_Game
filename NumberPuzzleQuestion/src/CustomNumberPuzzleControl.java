f04d295e723b9f81e94ee6a90857bd109732eefa
kss682 <kss682@gmail.com>
2021-07-10 23:09:26 +0530

    Logic added to CustomNumberPuzzleControl

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -36,27 +36,173 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
 		
-		//Your logic here		
+		//Your logic here
+		/*
+		 * 		
+		 * 		0  1  2  3 
+		 * 		4  5  6  7
+		 * 		8  9  10 11
+		 * 		12 13 14 15
+		 */
+		// if at pos 1 and 2 it can move left, right and bottom
+		if(emptyCellId == 1 || emptyCellId == 2) { 
+			if(buttonClicked == buttons[emptyCellId+1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId+4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 4;
+			}
+		}
+		// if pos at 7 and 11 it can move up, down and left
+		else if(emptyCellId == 7 || emptyCellId == 11) { 
+			if(buttonClicked == buttons[emptyCellId-1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 4;
+			}
+			else if(buttonClicked == buttons[emptyCellId+4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 4;
+			}
+		}
+		// if pos at 13 and 14 it can mov left, right and up.
+		else if(emptyCellId == 13 || emptyCellId == 14) { 
+			if(buttonClicked == buttons[emptyCellId+1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 4;
+			}
+		}
+		//if pos at 4 and 8 it can move up, down and right.
+		else if(emptyCellId == 4 || emptyCellId == 8) { 
+			if(buttonClicked == buttons[emptyCellId+1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 4;
+			}
+			else if(buttonClicked == buttons[emptyCellId+4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 4;
+			}
+		}
+		else if(emptyCellId == 5 || emptyCellId == 6 || emptyCellId == 9 || emptyCellId == 10) { 
+			if(buttonClicked == buttons[emptyCellId+1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId+4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 4;
+			}
+			else if(buttonClicked == buttons[emptyCellId-4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 4;
+			}
+		}
+		else if(emptyCellId == 0) {
+			if(buttonClicked == buttons[emptyCellId+1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId+4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 4;
+			}
+		}
+		else if(emptyCellId == 3) {
+			if(buttonClicked == buttons[emptyCellId-1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId+4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 4;
+			}
+		}
+		else if(emptyCellId == 15) {
+			if(buttonClicked == buttons[emptyCellId-1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 4;
+			}
+		}
+		else if(emptyCellId == 12) {
+			if(buttonClicked == buttons[emptyCellId+1]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId += 1;
+			}
+			else if(buttonClicked == buttons[emptyCellId-4]) {
+				swapButton(buttons[emptyCellId], buttonClicked);
+				emptyCellId -= 4;
+			}
+		}
 		
 		return emptyCellId;
 
 	}
+	
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
 		
 		//Your logic here
-		int a = getRandomNumber();
+		int a, i = 0;
+		boolean[] check = new boolean[16];
 		
 		
+		while(i<15) {
+			a = getRandomNumber();
+			if(a >= 1 && a<=15) {
+				if(check[a] == false) {
+					arr[i] = a;
+					i++;
+					check[a]=true;
+//					System.out.printf("%d ", a);
+				}
+			}
+		}
 		return arr;
 	}
+	
 	public boolean checkForWinner(Button[] buttons)
 	{
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
+		int[] arr = new int[15];
+		arr = getIntegerArrayOfButtonIds(buttons);
+		for(int i=1; i<15; i++) {
+			if(arr[i-1]>arr[i]) {
+				return false;
+			}
+		}
+		
+		
 		return winner;
 	}
 }
